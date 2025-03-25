4e845b35c1ae796b390b40735394bcda7c6b4bda
mk <moonykun.k.yang@foxconn.com>
2025-03-25 16:38:51 +0800

    🔧 build: github page build

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ["vue", "@vueuse/core", "pinia", "vue-router", "vue-i18n"],
+      resolvers: [
+        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式),组件中免导入
+        ElementPlusResolver(),
+      ],
+      // 是否在 vue 模板中自动导入
+      vueTemplate: true,
+      // 指定自动导入函数TS类型声明文件路径 (false:关闭自动生成)
+      //dts: false,
+      dts: "auto-imports.d.ts",
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

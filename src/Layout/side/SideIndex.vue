d1c88f6a00f8ebaa7d5dbe680de1910169988349
mk <moonykun@outlook.com>
2025-03-26 16:45:18 +0800

    🎈 perf: delete some file

--- src/Layout/side/SideIndex.vue
+++ src/Layout/side/SideIndex.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import avatarImage from '../../assets/image/avatar.jpg'
+</script>
 
 <template>
-  <el-avatar :size="50" src="src\assets\avatar.jpg" />
-  <el-icon><Watermelon /></el-icon>
+  <div class="side-info">
+    <el-avatar :size="50" :src="avatarImage" />
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import IconsResolver from 'unplugin-icons/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,27 +13,17 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     AutoImport({
-      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
       imports: ["vue", "@vueuse/core", "pinia", "vue-router", "vue-i18n"],
       resolvers: [
-        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式),组件中免导入
         ElementPlusResolver(),
-        IconsResolver({
-          prefix: 'Icon',
-        }),
       ],
-      // 是否在 vue 模板中自动导入
       vueTemplate: true,
-      // 指定自动导入函数TS类型声明文件路径 (false:关闭自动生成)
-      //dts: false,
       dts: "auto-imports.d.ts",
     }),
     Components({
       resolvers: [
-        IconsResolver({
-          enabledCollections: ['ep'],
-        }),
-        ElementPlusResolver()],
+        ElementPlusResolver(),
+      ],
     }),
   ],
   base: './',
